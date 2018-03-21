dadd010b61b40a55b1dd139653af16b7713100ab
Pankaj Shukla <32461516+shubhspcl1@users.noreply.github.com>
2018-03-21 23:41:22 +0530

    Indentation

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){  $("nav a,.up,.down").click(function(){
-var x = $(this).attr("data-section");
-$('html,body').animate({scrollTop:$(x).offset().top},1000);
-});
+$(document).ready(function(){  
+  $("nav a,.up,.down").click(function(){
+    var x = $(this).attr("data-section");
+    $('html,body').animate({scrollTop:$(x).offset().top},1000);
+  });
 });
